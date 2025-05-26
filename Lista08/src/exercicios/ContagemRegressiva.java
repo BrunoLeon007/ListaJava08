bc4708cc0512017c8e36b6f87dcc93a4dfa927c2
BrunoLeon007 <brunob734@gmail.com>
2025-05-26 12:24:10 -0300

    Add files via upload

---
+++ Lista08/src/exercicios/ContagemRegressiva.java
@@ -0,0 +1,24 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ContagemRegressiva {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Insira um numero para que a contagem regressiva se inicie:");
+		
+		int numero = sc.nextInt();
+		
+		
+		for(int regressiva = numero; numero >= regressiva; numero --);
+		
+		System.out.println(numero);
+		
+		System.out.println("Contagem finalizada");
+		
+		
+sc.close();
+;	}
+
+}
---
+++ Lista08/src/exercicios/ExibirNumeros1a10for.java
@@ -0,0 +1,12 @@
+package exercicios;
+
+public class ExibirNumeros1a10for {
+
+	public static void main(String[] args) {
+	for(int numero =1; numero <= 10; numero ++) {
+		System.out.println(numero);
+	}
+
+	}
+
+}
